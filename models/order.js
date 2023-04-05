df54371d869172e983bcf7550d81668216857920
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-05 11:58:06 +0200

    Relaciones OrderProduct

--- models/order.js
+++ models/order.js
@@ -3,27 +3,26 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
+  class Order extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Product.belongsTo(models.Category); 
-      Product.belongsToMany(models.Order,{
+      Order.belongsTo(models.User); 
+
+      Order.belongsToMany(models.Product,{
         through:models.OrderProduct
       })
     }
   }
-  Product.init({
-    serial_number: DataTypes.STRING,
-    name_product: DataTypes.STRING,
-    price_product: DataTypes.FLOAT,
-    CategoryId: DataTypes.INTEGER
+  Order.init({
+    amount: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Product',
+    modelName: 'Order',
   });
-  return Product;
+  return Order;
 };
